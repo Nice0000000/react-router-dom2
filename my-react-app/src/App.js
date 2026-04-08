d3fd9d90f435645662f343c08744bdc79615c1a9
Nice0000000 <yarikgg3900@gmail.com>
2026-04-08 21:59:48 +0300

    Clean up comments in App.js
    Removed commented check for Navbar file existence.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'; // Перевірте, чи файл у папці components
+import Navbar from './components/Navbar'; 
 import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
     <div className="App">
-      {/* Меню навігації буде відображатися на всіх сторінках */}
+
       <Navbar /> 
 
-      {/* Тут контент буде змінюватися залежно від адреси */}
       <Routes>
         <Route path="/" element={<h1>Головна сторінка</h1>} />
         <Route path="/contact" element={<ContactPage />} />
